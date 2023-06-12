426e9deba06f38253d6b37577100cbb2f92fc5ec
chranychaul <130452965+chranychaul@users.noreply.github.com>
2023-06-12 09:26:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+let humedad = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P2))
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.temperature() + 273)
+})
+input.onButtonPressed(Button.B, function () {
+    humedad = pins.digitalReadPin(DigitalPin.P1)
+    radio.sendNumber(pins.digitalReadPin(DigitalPin.P2))
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P2))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P1))
+    basic.pause(1000)
+    basic.clearScreen()
+})
